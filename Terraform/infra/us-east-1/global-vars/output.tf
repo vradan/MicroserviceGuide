498634c680537e84926d22827ed6d22e9934a183
root <root@localhost.localdomain>
2017-07-06 01:04:56 -0300

    Added etcd token info

--- Terraform/infra/us-east-1/global-vars/output.tf
+++ Terraform/infra/us-east-1/global-vars/output.tf
@@ -2,3 +2,5 @@ output "default-region" { value = "${var.default-region}" }
 output "s3-bucket" { value = "${var.s3-bucket}" }
 output "s3-bucket-path" { value = "${var.s3-bucket-path}" }
 output "coreos-stable-ami" { value = "${var.coreos-stable-ami}" }
+output "keypair" { value = "${var.keypair}" }
+output "etcd-token" { value = "${var.etcd-token}" }
--- Terraform/infra/us-east-1/global-vars/variables.tf
+++ Terraform/infra/us-east-1/global-vars/variables.tf
@@ -2,3 +2,5 @@ variable "default-region" { default = "us-east-1" }
 variable "s3-bucket" { default = "vradan.com" }
 variable "s3-bucket-path" { default = "MicroserviceGuide/Terraform/us-east-1"}
 variable "coreos-stable-ami" { default = "ami-a2577cb4" }
+variable "keypair" { default = "vradan-kp" }
+variable "etcd-token" { default = "https://discovery.etcd.io/7129bed2c17b6f6e4596f19522fb1c21" }
