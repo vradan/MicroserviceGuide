d448965dc3a4295e1d1ccf8e70ea8688f35e54df
Lucas Morinigo <lucas.morinigo.94@gmail.com>
2017-07-06 02:43:47 +0000

    Global variables configuration

---
+++ Terraform/infra/us-east-1/global-vars/output.tf
@@ -0,0 +1,4 @@
+output "default-region" { value = "${var.default-region}" }
+output "s3-bucket" { value = "${var.s3-bucket}" }
+output "s3-bucket-path" { value = "${var.s3-bucket-path}" }
+output "coreos-stable-ami" { value = "${var.coreos-stable-ami}" }
---
+++ Terraform/infra/us-east-1/global-vars/variables.tf
@@ -0,0 +1,4 @@
+variable "default-region" { default = "us-east-1" }
+variable "s3-bucket" { default = "vradan.com" }
+variable "s3-bucket-path" { default = "MicroserviceGuide/Terraform/us-east-1"}
+variable "coreos-stable-ami" { default = "ami-a2577cb4" }
