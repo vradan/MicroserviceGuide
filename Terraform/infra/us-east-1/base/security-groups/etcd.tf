967759b5ec9c29d10462b50f84fe0062bec6a0ef
Lucas Morinigo <lucas.morinigo.94@gmail.com>
2017-07-06 02:54:24 +0000

    Changed SSH ip

--- Terraform/infra/us-east-1/base/security-groups/etcd.tf
+++ Terraform/infra/us-east-1/base/security-groups/etcd.tf
@@ -14,7 +14,7 @@ resource "aws_security_group" "etcd-sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-	cidr_blocks = ["189.16.15.81/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
