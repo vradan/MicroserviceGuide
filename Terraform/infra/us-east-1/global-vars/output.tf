7632c9502558468fb1745a7b4414b45d8d555d24
Lucas Morinigo <lucas.morinigo.94@gmail.com>
2017-07-08 04:05:36 +0000

    Changed way to get etcd token, from script variable for environment variable

--- Terraform/infra/us-east-1/global-vars/output.tf
+++ Terraform/infra/us-east-1/global-vars/output.tf
@@ -3,4 +3,4 @@ output "s3-bucket" { value = "${var.s3-bucket}" }
 output "s3-bucket-path" { value = "${var.s3-bucket-path}" }
 output "coreos-stable-ami" { value = "${var.coreos-stable-ami}" }
 output "keypair" { value = "${var.keypair}" }
-output "etcd-token" { value = "${var.etcd-token}" }
+output "etcd-token" { value = "${var.etcd_token}" }
--- Terraform/infra/us-east-1/global-vars/variables.tf
+++ Terraform/infra/us-east-1/global-vars/variables.tf
@@ -3,4 +3,4 @@ variable "s3-bucket" { default = "vradan.com" }
 variable "s3-bucket-path" { default = "MicroserviceGuide/Terraform/us-east-1"}
 variable "coreos-stable-ami" { default = "ami-a2577cb4" }
 variable "keypair" { default = "vradan-kp" }
-variable "etcd-token" { default = "https://discovery.etcd.io/7129bed2c17b6f6e4596f19522fb1c21" }
+variable "etcd_token" {}
