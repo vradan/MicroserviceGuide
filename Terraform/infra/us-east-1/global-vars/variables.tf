28eaa48e5fc4b60c3e8678fbb6181bcc0ded15a5
Lucas Morinigo <lucas.morinigo.94@gmail.com>
2017-07-20 13:22:22 +0000

    Changed etcd token

--- Terraform/infra/us-east-1/global-vars/variables.tf
+++ Terraform/infra/us-east-1/global-vars/variables.tf
@@ -3,4 +3,4 @@ variable "s3-bucket" { default = "vradan.com" }
 variable "s3-bucket-path" { default = "MicroserviceGuide/Terraform/us-east-1"}
 variable "coreos-stable-ami" { default = "ami-a2577cb4" }
 variable "keypair" { default = "vradan-kp" }
-variable "etcd_token" { default = "https://discovery.etcd.io/cacfcee5f6dc9f932a4c10f7d0ea446b"  }
+variable "etcd_token" { default = "https://discovery.etcd.io/e40275c6bbccb689333d2e422c203c18"  }
