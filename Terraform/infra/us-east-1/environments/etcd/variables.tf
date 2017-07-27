b65e604fd791ef4aaa832ec163119e34ebd51fb5
Lucas Morinigo <lucas.morinigo.94@gmail.com>
2017-07-27 18:24:36 +0000

    Changed etcd machines size to consume free tier

--- Terraform/infra/us-east-1/environments/etcd/variables.tf
+++ Terraform/infra/us-east-1/environments/etcd/variables.tf
@@ -1,4 +1,4 @@
 variable "count" { default = 3 }
-variable "instance-type" { default = "t2.nano" }
+variable "instance-type" { default = "t2.micro" }
 variable "volume-type" { default = "gp2" }
 variable "volume-size" { default = "8" }
